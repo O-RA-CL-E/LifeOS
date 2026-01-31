d708681351a2bb504c2c75469d9a9b53aed443f3
ORΔCLΣ <154529081+O-RA-CL-E@users.noreply.github.com>
2026-01-31 13:07:08 +0100

    Add login view and implement basic routing in index.php

--- public/index.php
+++ public/index.php
@@ -1,10 +1,15 @@
 <?php
-
 require_once __DIR__ . '/../app/config/config.php';
+$db = require __DIR__ . '/../app/config/database.php';
 
-try {
-    $db = require __DIR__ . '/../app/config/database.php';
-    echo "Connexion à la base OK";
-} catch (Exception $e) {
-    echo "Erreur DB";
+// Simple routing minimal
+$page = $_GET['page'] ?? 'login';
+
+switch($page) {
+    case 'login':
+        require __DIR__ . '/../app/views/auth/login.php';
+        break;
+    default:
+        echo "Page not found";
+        break;
 }
